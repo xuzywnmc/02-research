62580c9a15e27ca10e89be856c87723d9ee1822c
bananatreelookbajiao <1519744291@qq.com>
2019-08-08 20:56:54 +0800

    "dating"

--- 06 practice-myself/digital image processing/CH4 filtering in Frequency Domain/Fourierseries01.m
+++ 06 practice-myself/digital image processing/CH4 filtering in Frequency Domain/Fourierseries01.m
@@ -6,8 +6,15 @@ title('Subplot 1: 原图')
 
 %F=fft2(f,P,Q)
 %以便结果函数大小PxQ
-F=fft2(f);
+F=fft2(f);%F只是傅里叶变换
 S=abs(F);%傅里叶频谱可以使用abs函数获得
 subplot(2,2,2);
 imshow(S,[]);
-title('Subplot 2: 原图')
+title('Subplot 2: 傅里叶变换的频谱')
+
+Fc=fftshift(F);
+%Fc是已经居中变换 fftshift就是将变换
+%的原点移动到频率矩形中心
+subplot(2,2,3);
+imshow(abs(Fc),[]);
+title('Subplot 3: 将频谱移到中心')
