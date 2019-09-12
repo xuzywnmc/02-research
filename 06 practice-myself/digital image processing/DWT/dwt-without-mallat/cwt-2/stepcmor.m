f0783a7cf11d724d13f3817b3f30ee74d35c21c7
bananatreelookbajiao <1519744291@qq.com>
2019-09-12 08:33:44 +0800

    "dating"

--- 06 practice-myself/digital image processing/DWT/dwt-without-mallat/cwt-2/stepcmor.m
+++ 06 practice-myself/digital image processing/DWT/dwt-without-mallat/cwt-2/stepcmor.m
@@ -1,11 +1,17 @@
 fs=2^6;    %采样频率
 dt=1/fs;    %时间精度
-timestart=-8;
-timeend=8;
+timestart=-140;
+timeend=140;
 t=(0:(timeend-timestart)/dt-1)*dt+timestart;
 L=length(t);
-z=4*sin(5*pi*linspace(6,12,L).*t);
 
+z=4*sin(2*pi*linspace(6,12,L).*t);
+
+% z=sin(20*t);
+% fli= fft(z)
+% f2= (0:length(fli)-1)*50/length(fli);
+% figure('NumberTitle', 'off', 'Name', '原函数傅里叶变换结果');
+% plot(f2,abs(fli))
 %定义计算范围和精度
 fmin=2;
 fmax=20;
@@ -29,12 +35,14 @@ iter=10;%小波函数的区间划分精度
 xWAV=linspace(-tlow2low,tlow2low,2^iter);
 stepWAV = xWAV(2)-xWAV(1);
 val_WAV=cumsum(cmorwavf(-tlow2low,tlow2low,2^iter,1,3))*stepWAV;
+% figure(99);
+% plot(val_WAV);
 % %卷积前准备
 xWAV = xWAV-xWAV(1);
  xMaxWAV = xWAV(end);
 coefs     = zeros(length(scal),L);%预初设coefs
 %4小波与信号的卷积
-for k = 1:length(scal)    %一个scal一行
+for k = 1:length(scal)   %一个scal一行
      a_SIG = scal(k); %a是这一行的尺度函数
     j = 1+floor((0:a_SIG*xMaxWAV)/(a_SIG*stepWAV));
 %         %j的最大值为是确定的，尺度越大，划分的越密。相当于把一个小波拉伸的越长。
@@ -42,10 +50,16 @@ for k = 1:length(scal)    %一个scal一行
 %         
    % end
     waveinscal = fliplr(val_WAV(j));%把积分值扩展到j区间，然后左右颠倒。f为当下尺度的积分小波函数
+%     if k==100,figure(100),plot(waveinscal);end
+%     figure(k);
+%     plot(val_WAV(j));
     %5 最重要的一步 wkeep1取diff(wconv1(ySIG,f))里长度为lenSIG的中间一段
 % % % %     conv(ySIG,f)卷积。
-    dd=diff(conv2(z,waveinscal, 'full'));
-    coefs(k,:) = -sqrt(a_SIG)*wkeep1(diff(conv2(z,waveinscal, 'full')),L);
+    coefs(k,:) = -sqrt(a_SIG)*wkeep1(diff(conv2(z,waveinscal, 'full')),L);  
+    %   C = CONV2(A, B) performs the 2-D convolution of matrices A and B.
+%   If [ma,na] = size(A), [mb,nb] = size(B), and [mc,nc] = size(C), then
+%   mc = max([ma+mb-1,ma,mb]) and nc = max([na+nb-1,na,nb]).
+%
 % % %       Y = WKEEP1(X,L,OPT) extracts the vector Y 
 % % %   from the vector X. The length of Y is L.
 % % %   If OPT = 'c' ('l' , 'r', respectively), Y is the central
@@ -54,11 +68,32 @@ for k = 1:length(scal)    %一个scal一行
 %卷积后图像大小：N1+N2-1 x N1+N2-1
     %
 end
-% %%
+% %coefs2=cwt_cmor(z,1,3,f,fs);
+% figure;
+% pcolor(t,f,abs(coefs));shading interp、
+%%
+figure('NumberTitle', 'off', 'Name', '作者编写的小波变换结果');
+pcolor(t,f,abs(coefs));shading interp
+% pcolor(t,f,abs(coefs));shading interp
+%快速小波变换结果
+figure('NumberTitle', 'off', 'Name', '基于cwt函数结果');
+[wt,f1,coi] = cwt(z,scal,'amor',fs);
+pcolor(t,f1,abs(wt));shading interp
+%%基于作者编写的三维图结果
 x=t;
 y=f;
 z=abs(coefs);
-figure;
+figure('NumberTitle', 'off', 'Name', '基于作者编写的三维图结果');
+mesh(x,y,z)
+xlabel('time');
+ylabel('scale');
+zlabel('amplitude');
+%%
+%%基于作者编写的三维图结果
+x=t;
+y=f1;
+z=abs(wt);
+figure('NumberTitle', 'off', 'Name', '基于cwt三维图结果');
 mesh(x,y,z)
 xlabel('time');
 ylabel('scale');
@@ -78,6 +113,3 @@ zlabel('amplitude');
 % ylabel('自变量2');
 % zlabel('因变量Z');
 %,
-% %coefs2=cwt_cmor(z,1,3,f,fs);
-% figure;
-% pcolor(t,f,abs(coefs));shading interp
